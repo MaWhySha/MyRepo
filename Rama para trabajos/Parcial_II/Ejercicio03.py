1ae7ed405eafb27a294315753565904a83e99cc0
MaWhySha <mawhysha@gmail.com>
2026-04-10 02:04:27 -0600

    Parcial 02 Fundamentos de programación, [Ejercicio 3] {Uso de removeprefix() y upper() para modificar formato de texto}

---
+++ Rama para trabajos/Parcial_II/Ejercicio03.py
@@ -0,0 +1,17 @@
+# Ejercicio 3: Uso de removeprefix() y upper ()
+
+# 1. Crear la variable con el texto
+texto = "ING.Carlos Fernando Guardado García"
+
+# 2. Remover el prefijo "ING."
+texto_sin_prefijo = texto.removeprefix("ING.")
+
+# 3. Convertir el texto restante a mayúsculas 
+texto_final = texto_sin_prefijo.upper()
+
+# Mostrar resultados 
+print("Texto original:", texto)
+print("Texto sin prefijo:", texto_sin_prefijo)
+print("Texto final:", texto_final)
+
+#MaWhySha
