c423975fdf16f04c77ac0ac432c8a1a94f641efe
MaWhySha <mawhysha@gmail.com>
2026-04-10 01:55:33 -0600

    Parcial 02 Fundamentos de programación, [Ejercicio 1] {Uso de strip() y count() para limpiar texto y contar caracteres}

---
+++ Rama para trabajos/Parcial_II/Ejercicio01.py
@@ -0,0 +1,17 @@
+# Ejercicio 1: Uso de strip() y count()
+
+# 1. Declarar la variable
+texto = "  elefante  "
+
+# 2. Eliminar espacios en blanco al inicio y al final
+texto_limpio = texto.strip()
+
+# 3. Contar cuántas veces aparece la letra "e"
+cantidad_e = texto_limpio.count("e")
+
+# Mostrar resultados
+print("Texto original:", texto)
+print("Texto limpio:", texto_limpio)
+print("Cantidad de 'e':", cantidad_e)
+
+#MaWhySha
