fb46c451a0316c0ff6fc2411c2b1ad77a6fd82a8
MaWhySha <mawhysha@gmail.com>
2026-04-10 02:15:15 -0600

    Parcial 02 Fundamentos de programación, [Ejercicio 9] {Uso de replace() y upper() para reemplazo de texto y conversión a mayúsculas}

---
+++ Rama para trabajos/Parcial_II/Ejercicio09.py
@@ -0,0 +1,17 @@
+# Ejercicio 9: Uso de replace() y upper()
+
+# 1. Cadena inicial
+texto = "Any time y Anytime"
+
+# 2. Reemplazar "Any time" por "Always"
+texto_reemplazado = texto.replace("Any time", "Always")
+
+# 3. Convertir todo a mayúsculas
+texto_final = texto_reemplazado.upper()
+
+# Mostrar resultados 
+print("Texto original:", texto)
+print("Texto reemplazado:", texto_reemplazado)
+print("Texto final:", texto_final)
+
+#MaWhySha
