3997afdd9331b02fcf944c1b31975ac709d89897
MaWhySha <mawhysha@gmail.com>
2026-04-27 00:37:25 -0600

    Ejercicio 6 semana 12: Identificación del día de la semana según número

---
+++ Rama para trabajos/Semana12/Ejercicio06.py
@@ -0,0 +1,26 @@
+# Ejercicio 6: Día de la semana
+
+try:
+    numero = int(input("Ingresa un número del 1 al 7: "))
+
+    if numero == 1:
+        print("Lunes")
+    elif numero == 2:
+        print("Martes")
+    elif numero == 3:
+        print("Miércoles")
+    elif numero == 4:
+        print("Jueves")
+    elif numero == 5:
+        print("Viernes")
+    elif numero == 6:
+        print("Sábado")
+    elif numero == 7:
+        print("Domingo")
+    else:
+        print("Error: Número fuera de rango")
+
+except ValueError:
+    print("Error: Debes ingresar un número válido")
+
+# MaWhySha
