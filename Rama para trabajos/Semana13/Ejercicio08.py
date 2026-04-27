cc7e3600fdae4c95ed3ff5441ecf30a6c94a4a82
MaWhySha <mawhysha@gmail.com>
2026-04-27 00:14:52 -0600

    Ejercicio 8 semana 13: Patrón de asteriscos

---
+++ Rama para trabajos/Semana13/Ejercicio08.py
@@ -0,0 +1,18 @@
+# Ejercicio 8: Patrón de asteriscos
+
+while True:
+    try:
+        n = int(input("Ingresa un número (0 para salir): "))
+
+        if n == 0:
+            print("Programa finalizado")
+            break
+
+        for i in range(1, n + 1):
+            if i % 2 != 0:  
+                print("*" * i)
+
+    except ValueError:
+        print("Error: Debes ingresar un número válido")
+
+# MaWhySha
